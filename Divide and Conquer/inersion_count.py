706079d09e7cc02669f96cde1db4285561c15bdf
fsiddh <jasonshaw546@gmail.com>
2020-09-07 11:15:35 +0530

    inversion count completed

--- Divide and Conquer/inersion_count.py
+++ Divide and Conquer/inersion_count.py
@@ -15,25 +15,24 @@ def merge(A, left, mid, right):
     inv_count = 0
 
     while i<=mid and j<=right:
-        if A[i]<A[j]:
+        if A[i]<=A[j]:
             B[k] = A[i]
             i += 1
-            k += 1
         else:
             B[k] = A[j]
             j += 1
-            k += 1
-            inv_count += (mid-i)
+            inv_count=inv_count+(mid-i + 1)
+        k += 1
 
-        while i <= mid:
-            B[k] = A[i]
-            i += 1
-            k +=1
+    while i <= mid:
+        B[k] = A[i]
+        i += 1
+        k +=1
 
-        while j <= right:
-            B[k] = A[j]
-            j += 1
-            k += 1
+    while j <= right:
+        B[k] = A[j]
+        j += 1
+        k += 1
 
     for i in range(left, right+1):
         A[i] = B[i]
@@ -43,4 +42,4 @@ def merge(A, left, mid, right):
 A = [1, 20, 6, 4, 5] 
 n = len(A) 
 result = mergeSort(A, 0, n-1) 
-print("Number of inversions are", result) 
+print(result) 
